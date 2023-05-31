5ac19d608bf6fa60dbef75818c2a0e868001a037
eunseol-kwak <heenmilk@naver.com>
2023-05-31 15:19:35 +0900

    Day2 HW

---
+++ java/src/day2/homework/Ex1.java
@@ -0,0 +1,13 @@
+package day2.homework;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		//Ex1) 콘솔에서 정수 2개와 문자(산술연산자)를 입력받아 출력하는 코드를 작성하세요.
+		
+		
+
+		
+	}
+
+}
---
+++ java/src/day2/homework/Ex2.java
@@ -0,0 +1,10 @@
+package day2.homework;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		//Ex2) 두 정수를 입력받아 두 정수의 산술연산 결과를 출력하는 코드를 작성하세요. 단, 2번째 숫자는 0이 아님. 
+
+	}
+
+}
