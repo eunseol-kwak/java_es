773e9e55736a3fa209c6e2b9c7804b86a09a2f73
eunseol-kwak <heenmilk@naver.com>
2023-06-01 08:59:59 +0900

    자바: 연습문제 p.69_Q5

---
+++ java/src/Exercise/Variable2.java
@@ -0,0 +1,15 @@
+package Exercise;
+
+public class Variable2 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		double num2 = 2.0;
+		
+		System.out.println(num1+(int)num2);
+		System.out.println(num1-(int)num2);
+		System.out.println(num1*(int)num2);
+		System.out.println(num1/(int)num2);
+	}
+
+}
