3f4034e15da833ffbba50df7d0189b0240eb846f
eunseol-kwak <heenmilk@naver.com>
2023-06-01 08:44:38 +0900

    Day2 HW

--- java/src/day2/homework/Ex2.java
+++ java/src/day2/homework/Ex2.java
@@ -8,20 +8,19 @@ public class Ex2 {
 		//Ex2) 두 정수를 입력받아 두 정수의 산술연산 결과를 출력하는 코드를 작성하세요. 단, 2번째 숫자는 0이 아님. 
 		Scanner sc = new Scanner(System.in);
 		
-		int num1 = sc.nextInt(); 
-		System.out.println(num1);
+		System.out.println("두 정수를 입력하세요.");
 		
-		int num1 = 1, num2 = 2;
-		int sum = num1 + num2;			
-		int sub = num1 - num2;
-		int mul = num1 * num2;
-		double div = (double)num1 / num2; //1/2의 결과가 0이어서, 1.0/2로 바꾸어 0.5가 나오게 함			int mod = num1 % num2;
-			
-		System.out.println("" + num1 + '+' + num2 + "=" + sum);			
-		System.out.println("" + num1 + '-' + num2 + "=" + sub);
-		System.out.println("" + num1 + '*' + num2 + "=" + mul);
-		System.out.println("" + num1 + '/' + num2 + "=" + div);			
-		System.out.println("" + num1 + '%' + num2 + "=" + mod);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println("" + num1 + '+' + num2 + "=" + (num1+num2));			
+		System.out.println("" + num1 + '-' + num2 + "=" + (num1-num2));
+		System.out.println("" + num1 + '*' + num2 + "=" + (num1*num2));
+		System.out.println("" + num1 + '/' + num2 + "=" + ((double)num1/num2));			
+		System.out.println("" + num1 + '%' + num2 + "=" + (num1%num2));
+		
+		sc.close();
+		
 	}
 
 }
