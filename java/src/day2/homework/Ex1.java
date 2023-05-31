f5717241ff83359399c82d071af62d9c7ae2c6e7
eunseol-kwak <134986592+eunseol-kwak@users.noreply.github.com>
2023-05-31 23:11:37 +0900

    Day2 HW

--- java/src/day2/homework/Ex1.java
+++ java/src/day2/homework/Ex1.java
@@ -1,12 +1,20 @@
 package day2.homework;
 
-public class Ex1 {
+import java.util.Scanner;
 
+public class Ex1 {	
+	
 	public static void main(String[] args) {
 		//Ex1) 콘솔에서 정수 2개와 문자(산술연산자)를 입력받아 출력하는 코드를 작성하세요.
+		Scanner sc = new Scanner(System.in);
 		
-		
+		System.out.println("두 정수와 산술연산자를 입력해주세요.");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt(); 
+		String str = sc.next();
+		System.out.println(num1 + str + num2);
 
+		sc.close();
 		
 	}
 
--- java/src/day2/homework/Ex2.java
+++ java/src/day2/homework/Ex2.java
@@ -1,10 +1,27 @@
 package day2.homework;
 
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
 		//Ex2) 두 정수를 입력받아 두 정수의 산술연산 결과를 출력하는 코드를 작성하세요. 단, 2번째 숫자는 0이 아님. 
-
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt(); 
+		System.out.println(num1);
+		
+		int num1 = 1, num2 = 2;
+		int sum = num1 + num2;			
+		int sub = num1 - num2;
+		int mul = num1 * num2;
+		double div = (double)num1 / num2; //1/2의 결과가 0이어서, 1.0/2로 바꾸어 0.5가 나오게 함			int mod = num1 % num2;
+			
+		System.out.println("" + num1 + '+' + num2 + "=" + sum);			
+		System.out.println("" + num1 + '-' + num2 + "=" + sub);
+		System.out.println("" + num1 + '*' + num2 + "=" + mul);
+		System.out.println("" + num1 + '/' + num2 + "=" + div);			
+		System.out.println("" + num1 + '%' + num2 + "=" + mod);
 	}
 
 }
