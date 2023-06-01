784600b89a95d67c03d6301f20254aebc0b3433e
eunseol-kwak <heenmilk@naver.com>
2023-06-01 14:58:54 +0900

    자바: if문 연습문제_ 음수양수 판별 예제

---
+++ java/src/day3/practice/Ex3.java
@@ -0,0 +1,39 @@
+package day3.practice;
+
+import java.util.Scanner;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+		/*Q: 정수 num을 입력받아서 num가 0인지, 음수인지, 양수인지를 판별하는 코드를 if문을 이용하여 작성하세요.
+		 *num가 0이면 0이라고 출력하고,
+		 *num가 음수이면 음수라고 출력하고,
+		 *num가 양수이면 양수라고 출력하는 코드를 작성하세요.
+		*/
+		
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input number : ");
+		num = sc.nextInt();
+		
+		//num가 0이다
+		//0과 같다
+		if (num == 0) {
+			System.out.println(0);
+		}
+		//num가 0보다 작다
+		//음수 => 0보다 작다
+		if (num < 0) {
+			System.out.println(num + " is a negative number");
+		}
+		//num가 0보다 크다
+		//양수 => 0보다 크다
+		if (num > 0) {
+			System.out.println(num + " is a positive number");
+		}
+		
+		sc.close();
+		
+	}
+
+}
