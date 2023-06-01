d08de999ed248af7813cb0705679f190fce811c1
eunseol-kwak <heenmilk@naver.com>
2023-06-01 14:57:54 +0900

    자바: if문 연습문제_홀짝 판별 예제

---
+++ java/src/day3/practice/Ex4_even.java
@@ -0,0 +1,29 @@
+package day3.practice;
+
+import java.util.Scanner;
+
+public class Ex4_even {
+
+	public static void main(String[] args) {
+		//정수 num를 입력받아 num가 홀수인지 짝수인지 판별하는 코드를 작성하세요.
+		//num를 2로 나누었을때 나머지가 0이 아니면 odd number라고 출력하고
+		//num를 2로 나누었을때 나머지가 0이면 even number라고 출력
+		
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input number : ");
+		num = sc.nextInt();
+		
+		//num를 2로 나누었을때 나머지가 0이 아니다.
+		if(num % 2 != 0) {
+			System.out.println(num + " is an even number");		
+		}
+		
+		//num를 2로 나누었을때 나머지가 0이다.
+		if(num % 2 == 0) {
+			System.out.println(num + " is an odd number");
+		}
+		sc.close();
+	}
+
+}
