01fcd1ffd002bdfd1a5bfdad384ddf1287f331dd
eunseol-kwak <heenmilk@naver.com>
2023-06-01 15:20:52 +0900

    Day3 HW

--- java/src/day3/homework/Ex1_conditional.java
+++ java/src/day3/homework/Ex1_conditional.java
@@ -1,10 +1,23 @@
 package day3.homework;
 
+import java.util.Scanner;
+
 public class Ex1_conditional {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*성별(M:남성, W:여성)를 입력받아 여성인지 확인하는 코드를 작성하세요.
+		 * 조건연산자 이용
+		 * input gender: M
+		 * Are you a woman? false
+		 * input gender: W
+		 * Are you a woman? true
+		 * */
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println("input gender: ");
+		
+	
 	}
 
 }
