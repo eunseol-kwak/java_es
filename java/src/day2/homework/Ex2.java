d5e64617bd2fd3b7a51b5ef9b064f265e5b7a5d8
eunseol-kwak <heenmilk@naver.com>
2023-06-01 09:50:43 +0900

    Day2 HW AnswerKey

--- java/src/day2/homework/Ex2.java
+++ java/src/day2/homework/Ex2.java
@@ -23,17 +23,20 @@ public class Ex2 {
 		int num1 = sc.nextInt(); //변수 선언과 동시에 입력을 받아서 초기화
 		int num2 = sc.nextInt();
 		// 두 정수의 산술 연산을 계산
-		int sum = num1 + num2;
+		//int sum = num1 + num2;
 		int sub = num1 - num2;
 		int mul = num1 * num2;
 		double div = (double)num1 / num2;
-		int mod = num1 % num2;
+		//int mod = num1 % num2;
 		//결과를 콘솔에 출력
-		System.out.println(num1 + " + " + num2 + " = " + sum);//("" + num1 + '+' + num2 + "=" + sum)		
-		System.out.println(num1 + " - " + num2 + " = " + sub);
+		//System.out.println(num1 + " + " + num2 + " = " + sum);		
+		System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+	    System.out.println(num1 + " - " + num2 + " = " + sub);
 		System.out.println(num1 + " * " + num2 + " = " + mul);
-		System.out.println(num1 + " / " + num2 + " = " + div);			
-		System.out.println(num1 + " % " + num2 + " = " + mod);
+		System.out.println(num1 + " / " + num2 + " = " + div);		
+		//System.out.println(num1 + " % " + num2 + " = " + mod);
+		//%는 +보다 우선순위가 높기 떄문에 ()을 생략해도 되지만 위에 +와 같은 형태로 보여주기 위해()를 붙임
+		System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
 		
 		sc.close();
 		
