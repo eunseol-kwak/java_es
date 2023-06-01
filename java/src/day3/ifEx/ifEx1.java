5b291cf4e8b663b7c1ebce857b298a797fa6d92f
eunseol-kwak <heenmilk@naver.com>
2023-06-01 14:59:31 +0900

    자바: if문 예제

---
+++ java/src/day3/ifEx/ifEx1.java
@@ -0,0 +1,26 @@
+package day3.ifEx;
+
+public class ifEx1 {
+
+	public static void main(String[] args) {
+		//정수 num가 0이면 0입니다라고 출력하는 코드를 작성하세요.
+		//조건문: 정수 num가 0이다
+		//정수 num가 0과 같다
+		int num = 10;
+		if(num == 0) {
+			//실행문: 0입니다라고 출력;
+			System.out.println("0 is 0");
+		}
+		
+		//정수 num가 0이 아니면 0이 아닙니다. 라고 출력하는 코드를 작성하세요.
+		//num가 0이 아니다
+		//num가 0과 같지 않다
+		if(num != 0) {
+			System.out.println(num + "is not 0");
+		//if문이 또있으면, 각각 수행한다. 
+			
+		}
+		
+	}
+
+}
