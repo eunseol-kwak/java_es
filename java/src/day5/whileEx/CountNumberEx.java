9e6f415188dcda911dda9e00b47a818ce139939c
eunseol-kwak <heenmilk@naver.com>
2023-06-05 15:00:29 +0900

    자바: while문 숫자출력 예제

---
+++ java/src/day5/whileEx/CountNumberEx.java
@@ -0,0 +1,17 @@
+package day5.whileEx;
+
+public class CountNumberEx {
+
+	public static void main(String[] args) {
+		/* 1부터 10까지 출력하는 코드를 작성하세요.
+		 * 반복횟수 : i는 1부터 i가 10까지//1이크면 크거나같다,10이크면 작거나같다// 1씩 증가
+		 * 규칙성 : i를 출력
+		 * */
+		 int i = 1; //i는 1부터		 
+		 while( i <= 10) { //i는 10까지 
+			 System.out.println(i);
+			 ++i; //i는 1씩 증가
+			 	
+		 }
+	}
+}
