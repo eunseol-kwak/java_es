6b4e90ba74d0d24fd0ff7554651df50864a37c22
eunseol-kwak <heenmilk@naver.com>
2023-06-05 15:02:13 +0900

    Day4 HW

--- java/src/day4/homework/NumberEx.java
+++ java/src/day4/homework/NumberEx.java
@@ -9,6 +9,7 @@ public class NumberEx {
 		 * num를 출력하는 코드를 작성하세요. 
 		*/
 		
+        /*
 		int num = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("input integer number: ");
@@ -21,18 +22,19 @@ public class NumberEx {
 		}
 	}
 }
-		/*Teacher's
+		*/
+		//Teacher's
 		int num;
 		Scanner sc = new Scanner(System.in);
 		
 		//정수 입력
 		System.out.println("input integer number: ");
-		num = nextInt();
+		num = sc.nextInt();
 		//정수 num가 짝수이면 num에 2를 나누고
 		//num2에 2를 나눈 값을 num에 저장
 		if(num % 2 == 0) {
 			num = num / 2; //(num /=2;)
-		
+		}
 		//아니면 num에 1을 더한 후
 		//num에 1을 더한 값을 num애 저장
 		else {
@@ -41,6 +43,6 @@ public class NumberEx {
 		//num를 출력
 		System.out.println(num);
 		sc.close();
-		 */
-		
+	}
+}	
 		
