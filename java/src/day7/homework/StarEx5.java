3a8b84d417076ce43c0547aac52f422adfce4f0d
eunseol-kwak <heenmilk@naver.com>
2023-06-08 14:44:49 +0900

    Day7 HW

---
+++ java/src/day7/homework/StarEx5.java
@@ -0,0 +1,22 @@
+package day7.homework;
+
+public class StarEx5 {
+
+	public static void main(String[] args) {
+		/*  다음과 같이 출력되도록 코드를 작성하세요.
+		 *      *               i=1      공백=4     *=1 i=*
+		 *     ***				i=2      공백=3     *=3 i+1		
+		 *    *****             i=3      공백=2     *=5 i+2
+		 *   *******            i=4      공백=1     *=7 i+3
+		 *    *****             i=3      공백=2     *=5
+		 * 	   ***              i=2      공백=3     *=3
+		 *      *               i=1      공백=4     *=1
+		 *                               공백=5-i   *=i+=1
+		 */	
+	
+							
+					
+		System.out.println();
+		}
+	}
+
