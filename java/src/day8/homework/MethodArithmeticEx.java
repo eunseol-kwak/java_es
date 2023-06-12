136163fd7554c220d6638f9422336cc3dda44090
eunseol-kwak <heenmilk@naver.com>
2023-06-13 08:39:17 +0900

    자바: 8일차 과제 - 산술연산자 예제 풀이

--- java/src/day8/homework/MethodArithmeticEx.java
+++ java/src/day8/homework/MethodArithmeticEx.java
@@ -34,7 +34,7 @@ public class MethodArithmeticEx {
 	/** 두 정수와 산술 연산자가 주어지면 산술 연산 결과를 알려주는 메서드
 	 * 매개변수: 두정수와 산술 연산자 => int num1, char op + int num2
 	 * 리턴타입: 산술연산결과 => 실수 => double
-	 * 메서드명: arithmethic
+	 * 메서드명: arithmetic
 	 */
 	
 	public static double arithmetic(int num1, char op, int num2) {
