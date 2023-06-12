7b1617724c9d1561fd2342a3be04c89a5c5c7c39
eunseol-kwak <heenmilk@naver.com>
2023-06-12 10:57:57 +0900

    자바 : 메소드 최소 공배수 예제(수정)

--- java/src/day8/practice/MethodLCMEx.java
+++ java/src/day8/practice/MethodLCMEx.java
@@ -14,14 +14,15 @@ public class MethodLCMEx {               //Git보고 다시해야함!!!!
 		 * */
 		int num1, num2;
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("input 2 numbers: ");
 		num1 = sc.nextInt();
 		num2 = sc.nextInt();
 		
-		int lcm = Lcm1(num1,num2);
+		int lcm = lcm1(num1,num2);
 		System.out.println(num1 + " and " + num2 + " lcm : " + lcm);
 		
-		lcm = Lcm2(num1,num2);
+		lcm = lcm2(num1,num2);
 		System.out.println(num1 + " and " + num2 + " lcm : " + lcm);
 				
 		sc.close();
@@ -31,8 +32,11 @@ public class MethodLCMEx {               //Git보고 다시해야함!!!!
 		 * 리턴타입: 최소 공배수 => 정수 => int
 		 * 메서드명: lcm
 		 */	
-	public static int lcm1(int num1, int num2, int num3) {
+	public static int lcm1(int num1, int num2) {
 		for(int i = num1; i <= num1 * num2; i += num1) {
+			//공배수를 찾음. isMultiple은 배수를 찾는 메서드이고,
+			//i가 num1의 배수들이기 때문에 i가 num2의 배수이면 num1과
+			//num2의 공배수가 됨
 			if(isMultiple(i, num2)) { 
 				return i;
 			}
@@ -42,14 +46,17 @@ public class MethodLCMEx {               //Git보고 다시해야함!!!!
 	public static int lcm2(int num1, int num2) {
 		return num1 * num2 / gcd(num1, num2);
 	}
+	
 	public static boolean isMultiple(int num1, int num2) {
 		return num1 % num2 == 0;
     }
+	
 	public static int gcd(int num1, int num2) {
 		for(int i = num1; i>=1; i--) {
 			if(isMultiple(num1,i) && isMultiple(num2, i)) {
 				return i;
 			}
-		}			
+		}
+		return 1;
 	}	
 }
