d13e78c1f2ff2a940dc2cdb9c21181bf2ef59834
eunseol-kwak <heenmilk@naver.com>
2023-06-14 15:14:11 +0900

    Day11 HW

--- java/src/day11/homework/BaseballGameEx.java
+++ java/src/day11/homework/BaseballGameEx.java
@@ -1,5 +1,7 @@
 package day11.homework;
 
+import java.util.Scanner;
+
 public class BaseballGameEx {
 
 	public static void main(String[] args) {
@@ -22,7 +24,35 @@ public class BaseballGameEx {
 		 * 3S
 		 * Good!
 		 * */
-
+		int min = 1, max = 9;
+		int [] arr = new int[3];
+		int num1, num2, num3;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input 3 nums: ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		num3 = sc.nextInt();
+		
+		
+		//방법1 : 9번지까지 있는 배열을 이용
+		//장점 : 작업이 단순 / 단점: 저장 공간이 필요
+		boolean checkArr[] = new boolean[10];
+		
+		for(int i = 0; i < 3; ) {
+			int random = (int)(Math.random() * (max - min + 1) + min);
+			//checkArr 배열에 random번지에 있는 값이 false이면
+			if(!checkArr[random]) {
+				//random을 배열에 저장 
+			    arr[i] = random;
+				//i를 증가
+			    i++;
+			    //checkArr배열에 random번지에 있는 값을 true로 변경
+			    checkArr[random] = true;
+			}
+			if()
+		}
+		for(int i = 0; i < 3; i++) {
+			System.out.println(arr[i]);
+		}
 	}
-
 }
