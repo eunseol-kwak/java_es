7b5fce2f2de642a3a5d7a15b43354b773e1d4338
eunseol-kwak <heenmilk@naver.com>
2023-06-15 14:49:41 +0900

    Day12 HW

---
+++ java/src/day12/homework/LottoEx.java
@@ -0,0 +1,29 @@
+package day12.homework;
+
+public class LottoEx {
+
+	public static void main(String[] args) {
+		/*로또 번호를 생성하고, 사용자가 번호를 입력하면 몇등인지 출력하는 코드를 작성하세요.
+		 * 1등: 번호 6개
+		 * 2등: 번호 5개 + 보너스
+		 * 3등: 번호 5개
+		 * 4등: 번호 4개
+		 * 5등: 번호 3개
+		 * number: [1, 3, 40, 23, 24, 45]
+		 * bonus: 2 //컴퓨터는 보너스 번호를 포함해서 7개 생성
+		 * user: 1 2 3 4 5 6 //사용자는 6개 생성
+		 * losing ticket!
+		 * */
+
+		
+		
+		// 맞춰야할 숫자 6개를 랜덤으로 생성
+		int min = 1, max = 45;
+		int [] user = new int[6];
+		// 중복 제거
+		// 반복문 
+			// com[7] user[6] 비교
+		//losing ticket
+	}
+
+}
