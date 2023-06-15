a8c0dfc8cabb840260ba84fe2e4b4e74cc12e018
eunseol-kwak <heenmilk@naver.com>
2023-06-15 14:48:39 +0900

    자바: 숫자 야구게임 과제 풀이

--- java/src/day11/homework/BaseballGameEx.java
+++ java/src/day11/homework/BaseballGameEx.java
@@ -2,6 +2,8 @@ package day11.homework;
 
 import java.util.Scanner;
 
+import array.Array;
+
 public class BaseballGameEx {
 
 	public static void main(String[] args) {
@@ -24,35 +26,121 @@ public class BaseballGameEx {
 		 * 3S
 		 * Good!
 		 * */
+		int com[] = new int[3];  //랜덤으로 생성된 수를 저장할 배열
+		int user[] = new int[3]; //사용자가 입력한 수를 저장할 배열
 		int min = 1, max = 9;
-		int [] arr = new int[3];
-		int num1, num2, num3;
+		int strike = 0; //스트라이크 개수
+		int ball = 0; //볼의 개수
 		Scanner sc = new Scanner(System.in);
-		System.out.println("input 3 nums: ");
-		num1 = sc.nextInt();
-		num2 = sc.nextInt();
-		num3 = sc.nextInt();
 		
-		
-		//방법1 : 9번지까지 있는 배열을 이용
-		//장점 : 작업이 단순 / 단점: 저장 공간이 필요
-		boolean checkArr[] = new boolean[10];
-		
-		for(int i = 0; i < 3; ) {
-			int random = (int)(Math.random() * (max - min + 1) + min);
-			//checkArr 배열에 random번지에 있는 값이 false이면
-			if(!checkArr[random]) {
-				//random을 배열에 저장 
-			    arr[i] = random;
-				//i를 증가
-			    i++;
-			    //checkArr배열에 random번지에 있는 값을 true로 변경
-			    checkArr[random] = true;
+		// 맞춰야할 숫자 3개를 랜덤으로 생성(중복 X)
+		Array.createRandomArray(min, max, com);
+		//Array.printArray(com); //나중에 주석 처리
+	
+		// 반복문 - 무조건 한번은 실행	
+		do {
+			// 사용자가 숫자 3개를 입력
+			System.out.print("user : ");
+			for(int i = 0; i < user.length; i++) {
+				user[i] = sc.nextInt();
 			}
-			if()
+			//사용자 입력 체크
+			if(Array.arrayCheck(user)) {
+				System.out.println("You must not enter the same number!");
+				continue;
+			}
+			// 판별
+			//strike = 0;
+			//ball = 0;
+			
+			//스트라이크 개수 판별
+			strike = strike(com,user);
+			//System.out.println(strike);
+			
+			//볼의 개수 판별
+			ball = ball(com,user);
+			//System.out.println(ball);
+			
+			//스트라이크와 볼의 개수에 맞게 출력
+			printResult(strike,ball);
+			
+		}while(strike < 3);
+		System.out.println("Good!");
+		sc.close();
+	}	
+	
+	/**스트라이크 개수를 판별하는 메서드
+	 * => 두 배열에서 같은 번지에 있는 값들이 몇개 같은지 알려주는 메서드
+	 * 매개변수: 두 배열 => int arr1[], int arr2[]
+	 * 리턴타입: 같은 번지에 있는 값들이 몇개 같은지 => 몇개 => 정수 => int
+	 * 메서드명: strike
+	 * */
+	public static int strike(int arr1[], int arr2[]) {
+		//배열 생성이 안된 배열이 1라도 있으면 비교할 수 없어서 0을 리턴/배열이 비어있는지 고려
+		if(arr1 == null || arr2 == null) {
+			return 0;
 		}
-		for(int i = 0; i < 3; i++) {
-			System.out.println(arr[i]);
+		//배열의 개수가 다른 경우 작은 크기의 배열을 기준으로 비교하기 위해 size계산/두개의 개수가 다를수 있는지 고려
+		// 1 2 3
+		// 4 5 6 7 /스트라이크는 오류, 볼은 상관없음
+		int size = arr1.length < arr2.length ? arr1.length : arr2.length;
+		int count = 0; //스트라이크 개수
+		for(int i = 0; i < size; i++) {
+			//같은 번지에 있는 값들이 서로 같으면(스트라이크이면 개수를 1증가)
+			if(arr1[i] == arr2[i]) {
+				count++;
+			}
 		}
+		return count;
+	}
+	/**볼 개수를 판별하는 메서드
+	 * => 두 배열에서 다른 번지에 있는 값들이 몇개 같은지 알려주는 메서드
+	 * 매개변수: 두 배열 => int arr1[], int arr2[]
+	 * 리턴타입: 같은 번지에 있는 값들이 몇개 같은지 => 몇개 => 정수 => int
+	 * 메서드명: ball
+	 * */
+	public static int ball(int arr1[], int arr2[]) {
+		if(arr1 == null || arr2 == null) {
+			return 0;
+		}
+		int count = 0;
+		//다른 번지를 비교해서 찾아도 되지만
+		//두 배열에서 같은 숫자의 개수에서 스트라이크 개수를 빼면 볼의개수
+		for(int tmp : arr1) {
+			//arr1에서 꺼낸 값이 arr2에 있으면 개수를 증가
+			if(Array.contains(arr2, tmp, arr2.length)) {
+				count++;
+			}
+		}
+		return count - strike(arr1, arr2);
+		//이중 반복문을 이용해서 다른 번지를 비교하는 코드
+		/*for(int i = 0; i < arr1.length; i++) {
+			for(int j = 0; j < arr2.length; j++) {
+				if(i == j) {
+					continue;
+				}
+				if(arr1[i] == arr2[i]) {
+					count++;
+				}
+			}
+		}
+		return count;
+		*/
+	} 
+	/**스트라이크와 볼의 개수가 주어지면 결과를 출력하는 메서드
+	 * 매개변수: 스트라이크 개수, 볼의 개수 => int strike, int ball
+	 * 리턴타입: 없음 => void
+	 * 메서드명: printResult */
+	public static void printResult(int strike, int ball) {
+		if(strike != 0) {
+			System.out.print(strike + "S");
+		}
+		if(ball != 0) {
+			System.out.println(ball + "B");
+		}
+		if(strike == 0 && ball == 0) {
+			System.out.print("30");
+		}
+		System.out.println();
 	}
 }
