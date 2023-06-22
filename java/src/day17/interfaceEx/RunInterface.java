b9a4dee0e2e32b4a449b3badc41d51c11c678a96
eunseol-kwak <heenmilk@naver.com>
2023-06-22 15:17:17 +0900

    자바: 판매관리 예제(인터페이스적용)-수정

--- java/src/day17/interfaceEx/RunInterface.java
+++ java/src/day17/interfaceEx/RunInterface.java
@@ -4,7 +4,7 @@ public interface RunInterface{
 		/*public static final */int NUM = 10;  //클래스 상수 => 클래스를 이용하여 호출(객체 생성없이 호출 가능)
 		/*public abstract 자동으로 추가*/ void run();
 		// 에러 void stop();
-		default void strop() {
+		default void stop() {
 			System.out.println("Program stop!");
 		}
 	}
