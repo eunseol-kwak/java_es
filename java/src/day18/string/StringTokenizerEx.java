34692af8ba4b68493cdbcd06d3ec36618195b12a
eunseol-kwak <heenmilk@naver.com>
2023-06-23 14:52:55 +0900

    자바: StringTokenizer  예제

---
+++ java/src/day18/string/StringTokenizerEx.java
@@ -0,0 +1,20 @@
+package day18.string;
+
+import java.util.StringTokenizer;
+
+public class StringTokenizerEx {
+
+	public static void main(String[] args) {
+		//split과 비슷하지만, 한가지 구분자만 사용하는것이 다름
+		
+		String fruits = "apple,orange,banana";
+		StringTokenizer st = new StringTokenizer(fruits, ",");
+		
+		//hasMoreTokens : 다음 토큰이 있는지 없는지 알려줌. 없다는건 끝났다는 의미
+		while(st.hasMoreTokens()) {
+			System.out.println(st.nextToken()); //nextToken : 토큰을 가져옴
+		}
+		
+	}
+
+}
