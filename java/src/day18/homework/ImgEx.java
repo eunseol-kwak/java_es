3ce487e2db894ff1fb39325303a9e7cbfd6948c7
eunseol-kwak <heenmilk@naver.com>
2023-06-23 14:53:37 +0900

    Day18 HW

---
+++ java/src/day18/homework/ImgEx.java
@@ -0,0 +1,32 @@
+package day18.homework;
+
+import java.util.Scanner;
+
+public class ImgEx {
+
+	public static void main(String[] args) {
+		/* 파일명을 입력받아 파일이 이미지인지 확인하는 코드를 작성하세요. 
+		 * 이미지 확장자 : jpg, bmp, png(더 있지만 간단하게 3개만 체크) 
+		 * input : dog.jpg
+		 * dog.jpg : Image
+		 * 
+		 * input : dog.exe
+		 * dog.exe : Not image */
+
+		//파일명 입력
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input: ");
+		String str = sc.nextLine();
+		
+		if(str == "jpg"|| str == "bmp" || str == "png") {
+			System.out.println(str + " : " + " Image");
+		}else { 
+			System.out.println(str + " : " + "Not image");
+		}
+		
+		
+		
+		sc.close();
+	}
+
+}
