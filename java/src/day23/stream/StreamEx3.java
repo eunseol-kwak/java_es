5d999afb47a3d44d53a4c56067f40fef022d54de
eunseol-kwak <heenmilk@naver.com>
2023-06-30 15:07:39 +0900

    자바 : 보조 스트림 예제

---
+++ java/src/day23/stream/StreamEx3.java
@@ -0,0 +1,26 @@
+package day23.stream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StreamEx3 {
+	//보조스트림
+	public static void main(String[] args) {
+		try(FileInputStream fis = new FileInputStream("test.txt");
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			InputStreamReader isr = new InputStreamReader(bis)){
+			char ch[] = new char[1024];
+			while(isr.read(ch) != -1) {
+				System.out.println(ch);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
