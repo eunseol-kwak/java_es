6fb7c9100ce43c03c4f7a17e071aa35ce1a75b6f
eunseol-kwak <heenmilk@naver.com>
2023-07-03 14:28:14 +0900

    자바 : 쓰레드 생성 예제

---
+++ java/src/day24/thread/ThreadEx2.java
@@ -0,0 +1,21 @@
+package day24.thread;
+
+public class ThreadEx2 {
+
+	public static void main(String[] args) {
+		Thread thread = new Thread(new ChildThread2());//매개변수를 생성자에 넣어줘야함
+		thread.start();
+		for(int i = 0; i < 10; i++) {
+			System.out.println("+");
+		}
+	}
+}
+
+class ChildThread2 implements Runnable{
+	@Override
+	public void run() {
+		for(int i = 0; i < 10; i++) {
+			System.out.println("-");
+		}
+	}
+}
