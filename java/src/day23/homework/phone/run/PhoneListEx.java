3aaafba3e55c05fd6c4062fc088594551b31ec3c
eunseol-kwak <heenmilk@naver.com>
2023-07-03 13:16:25 +0900

    Day23 HW

---
+++ java/src/day23/homework/phone/run/PhoneListEx.java
@@ -0,0 +1,14 @@
+package day23.homework.phone.run;
+
+import day23.homework.phone.controller.PhoneManager;
+
+public class PhoneListEx {
+
+	public static void main(String[] args) {
+		//전화번호 프로그램에 저장과 불러오기 기능 추가
+		PhoneManager pm = new PhoneManager();
+		pm.run();
+		
+	}
+
+}
